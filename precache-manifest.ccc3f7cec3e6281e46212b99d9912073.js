282fb635018e2a9de7ce01dbb2ccf53d78996fde
dvkoh <topdavid@naver.com>
2019-06-14 18:13:29 +0900

    Updates

---
+++ precache-manifest.ccc3f7cec3e6281e46212b99d9912073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056b949cc8f0ede71905a622bb3f6af4",
+    "url": "/booksbooksbooks/index.html"
+  },
+  {
+    "revision": "61aaf42886e304b02eaa",
+    "url": "/booksbooksbooks/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "a8fb86adc4e581aed588",
+    "url": "/booksbooksbooks/static/css/main.3e5b5811.chunk.css"
+  },
+  {
+    "revision": "61aaf42886e304b02eaa",
+    "url": "/booksbooksbooks/static/js/2.2c243ae8.chunk.js"
+  },
+  {
+    "revision": "a8fb86adc4e581aed588",
+    "url": "/booksbooksbooks/static/js/main.3b923c8f.chunk.js"
+  },
+  {
+    "revision": "9a8128d24e23000e66c2",
+    "url": "/booksbooksbooks/static/js/runtime~main.2772241e.js"
+  }
+]);
